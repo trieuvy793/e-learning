03a60f441c0613c5cc09926880bcee8e3b143ba2
Trieu Vy <trieuvygialai@gmail.com>
2024-03-02 17:35:52 +0700

    done login with google

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import LoginScreen from './App/Screen/LoginScreen';
 
 export default function App() {
   return (
+    <ClerkProvider publishableKey={"pk_test_dG9waWNhbC1jYXRmaXNoLTM1LmNsZXJrLmFjY291bnRzLmRldiQ"}>
     <View style={styles.container}>
-      <LoginScreen/>
+      
+      <SignedIn>
+          <Text>You are Signed in</Text>
+        </SignedIn>
+        <SignedOut>
+        <LoginScreen/>
+        </SignedOut>
     </View>
+    </ClerkProvider>
   );
 }
 
--- App/Screen/LoginScreen.js
+++ App/Screen/LoginScreen.js
@@ -1,10 +1,33 @@
 import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import * as WebBrowser from "expo-web-browser";
 import app from './../../assets/images/app.jpg'
 import Colors from '../Utils/Colors'
 import google from './../../assets/images/google.webp'
+import { useOAuth } from "@clerk/clerk-expo";
+import { useWarmUpBrowser } from "./../../hooks/useWarmUpBrowser";
 
+WebBrowser.maybeCompleteAuthSession();
 export default function LoginScreen() {
+  useWarmUpBrowser();
+ 
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+ 
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+ 
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+
   return (
     <View style={{display:'flex', alignItems:'center'}}>
       <Image source={app}
@@ -23,7 +46,9 @@ export default function LoginScreen() {
         fontSize:20, marginTop:20,
         color:Colors.LIGHT_PRIMARY}}>Your Ultimate Programming Learning</Text>
         
-        <TouchableOpacity style={{backgroundColor:Colors.WHITE,
+        <TouchableOpacity 
+        onPress={onPress}
+        style={{backgroundColor:Colors.WHITE,
         display:'flex', flexDirection:'row',
         alignItems:'center', gap:10,
         justifyContent:'center', padding:10,

eba7f128680c83a2b3f1033b547821aee33f0d1f
Trieu Vy <trieuvygialai@gmail.com>
2024-03-03 17:46:57 +0700

    Done HomeScreen Header

--- App/Screen/HomeScreen.js
+++ App/Screen/HomeScreen.js
@@ -1,10 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Header from '../Components/HomeScreen/Header.js'
+import Colors from '../Utils/Colors.js'
 
 export default function HomeScreen() {
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <View style={{backgroundColor:Colors.PRIMARY, height:250, padding:20}}>
+      <Header/>
+      </View>
+      
     </View>
   )
 }
